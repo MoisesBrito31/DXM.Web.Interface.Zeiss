4b5a130c2bee5ebfd80e705fc8a3070542864585
MoisesBrito31 <58660892+MoisesBrito31@users.noreply.github.com>
2020-01-29 17:54:57 -0300

    ajuste do javaAssinc

--- DXM.Web.Interface/Controllers/plantaController.cs
+++ DXM.Web.Interface/Controllers/plantaController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace DXM.Web.Interface.Controllers
 {
@@ -19,6 +20,16 @@ namespace DXM.Web.Interface.Controllers
             {
                 return RedirectToAction("offline","oee");
             }
+        }
+
+        [HttpGet]
+        public string conjunto_linhas_zeiss()
+        {
+            if (!Program.registrado) { return "falha"; }
+            string ret = "";
+            if (Program.oee.DXM_Status.Contains("OffLine")) { ret = "falha"; }
+            else { ret = JsonConvert.SerializeObject(Program.oee.Linhas); }
+            return ret;
         }
     }
 }
